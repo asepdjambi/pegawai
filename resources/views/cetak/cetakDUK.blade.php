b3e57850567e7a2a40a04a98de540f45b6732a3c
asepdjambi <asepsyaipudin85@gmail.com>
2021-07-26 21:41:48 +0700

    memperbaiki print DUK atau cetakDUK.blade.php

--- resources/views/cetak/cetakDUK.blade.php
+++ resources/views/cetak/cetakDUK.blade.php
@@ -7,9 +7,9 @@
 
     <link rel="stylesheet" href="admin/assets/dist/css/bootstrap.min.css">
 
-    <p style="text-align: center; font-size=16pt"><strong>DATA DUK DINAS PERHUBUNGAN </strong><strong>KABUPATEN BATANG HARI</strong></p>
+    <p style="text-align: center; font-size:16pt"><strong>DATA DUK DINAS PERHUBUNGAN </strong><strong>KABUPATEN BATANG HARI</strong></p>
     <span></span>
-    <table class="table table-bordered table-sm" style="font-size: 7pt; text-align:center; width:100%; cellspacing:15px">
+    <table class="table table-bordered table-sm" style="font-size:7pt; text-align:center; width:100%">
         <thead>
             <tr>
                 <th rowspan="2" style="vertical-align: middle; text-align:center">NO</th>
@@ -55,95 +55,152 @@
 
             @foreach ($pegawai as $peg)
 
-                <tr>
-                    <td style="vertical-align: middle">{{ $no++ }}</td>
-                    <td style="vertical-align: middle">{{ $peg->Nama }}</td>
-                    <td style="vertical-align: middle">{{ $peg->NIP }}</td>
-                    @php
-                    // $gol=App\Models\pns::where('pegawai_id','=',$peg->id)->first();
-                    @endphp
-                    {{-- <td style="vertical-align: middle">{{ $gol->golongan->gol }}</td> --}}
-                    <td style="vertical-align: middle"></td>
-                    <td style="vertical-align: middle">{{ $peg->pns->tmt_capeg->format('d-m-Y') }}</td>
-                    <td style="vertical-align: middle">{{ $peg->golongan->gol }}</td>
-                    @php
-                    // mengabil nilai akhir dari tabel
-                    $tmt1=DB::table('riwayat_gol')->where([
-                    ['pegawai_id','=',$peg->id],
-                    ])->max('tmt');
+            <tr>
+                <td style="vertical-align: middle">{{ $no++ }}</td>
+                <td style="vertical-align: middle">{{ $peg->Nama }}</td>
+                <td style="vertical-align: middle">{{ $peg->NIP }}</td>
+                @php
+                // $gol=App\Models\pns::where('pegawai_id','=',$peg->id)->first();
+                @endphp
+                {{-- <td style="vertical-align: middle">
+                @if ($gol->isNotEmpty())
+                    {{ $gol->golongan->gol }};
+                @else
+                #
+                @endif
+                </td> --}}
+                <td style="vertical-align: middle"></td>
 
-                    // $tmt2=$tmt1::max('tmt');
+                {{-- //menampilkan tmt Capeg --}}
+                @php
+                $capeg=\App\Models\pns::where('pegawai_id', '=', $peg->id)->get();
+                @endphp
+                <td style="vertical-align: middle">
+                    @if ($capeg->isNotEmpty())
+                    {{ $peg->pns->tmt_capeg->format('d-m-Y') }};
+                    @else
+                    #
+                    @endif
+                </td>
+                <td style="vertical-align: middle">{{ $peg->golongan->gol }}</td>
+                @php
+                // mengabil nilai akhir dari tabel
+                $tmt1=DB::table('riwayat_gol')->where([
+                ['pegawai_id','=',$peg->id],
+                ])->max('tmt');
 
-                    // DB::select('select * from riwayat_gol where pegawai_id ='.$peg->id, [1])
-                    @endphp
-                    {{-- merubah format menjadi 02-12-2020 tanpa protected $dates pada model --}}
-                    <td style="vertical-align: middle">{{ date('d-m-Y', strtotime($tmt1)) }}</td>
+                // $tmt2=$tmt1::max('tmt');
 
-                    <td style="vertical-align: middle">{{ $peg->jab }}</td>
-                    <td style="vertical-align: middle">{{ $peg->jabatan->eselon }}</td>
+                // DB::select('select * from riwayat_gol where pegawai_id ='.$peg->id, [1])
+                @endphp
+                {{-- merubah format menjadi 02-12-2020 tanpa protected $dates pada model --}}
+                <td style="vertical-align: middle">{{ date('d-m-Y', strtotime($tmt1)) }}</td>
 
-                    @php
-                    $jab1=DB::table('riwayat_jab')->where([
-                    ['pegawai_id','=',$peg->id],
-                    ])->max('tmt');
-
-                    $masker=App\Models\gajiberkala::where('pegawai_id','=',$peg->id)
-                    ->orderByDesc('id')
-                    ->limit(1)
-                    ->first();
-
-                    $dikjab=App\Models\teknis::where('pegawai_id','=',$peg->id)
-                    ->orderByDesc('id')
-                    ->limit(1)
-                    ->first();
-
-                    $dikformal=App\Models\pendidikan::where('pegawai_id','=',$peg->id)
-                    ->orderByDesc('id')
-                    ->limit(1)
-                    ->first();
-
-                    $now=now();//tanggal hari ini
-                    $t_lahir=$peg->tgl_L; //tanggal lahir
-                    $usia_tahun=$t_lahir->diffInYears($now); //menghitung usia dalam tahun
-                    $usia_bulan=$t_lahir->diffInMonths($now); //menghitung usia dalam bulan
-                    $usia_hari=$t_lahir->diffInDays($now);//menghitung usia dalam hari
-
-                    @endphp
-
-                    <td style="vertical-align: middle">{{ date('d-m-Y', strtotime($jab1)) }}</td>
-                    <td style="vertical-align: middle">Dinas Perhubungan</td>
-                    <td style="vertical-align: middle">{{ $peg->masa_kerja_t }}</td>
-                    <td style="vertical-align: middle">{{ $peg->masa_kerja_b }}</td>
-                    <td style="vertical-align: middle">{{ $dikjab->nama_t }}</td>
-                    <td style="vertical-align: middle">{{ $dikjab->tgl_m->format('Y.m') }}</td>
-                    <td style="vertical-align: middle">{{ $dikjab->lama }}</td>
-                    <td style="vertical-align: middle">{{ $dikformal->jurusan }}</td>
-                    <td style="vertical-align: middle">{{ $dikformal->tahun }}</td>
-                    <td style="vertical-align: middle">{{ $dikformal->gelar }}</td>
-                    <td style="vertical-align: middle">{{ $peg->JK }}</td>
-                    <td style="vertical-align: middle">{{ $usia_tahun }}</td>
-                    <td style="vertical-align: middle">{{ $usia_bulan }}</td>
-                    <td style="vertical-align: middle">{{ $peg->asal_ins }}</td>
-                    <td style="vertical-align: middle">#</td>
-                </tr>
+                <td style="vertical-align: middle">{{ $peg->jab }}</td>
+                <td style="vertical-align: middle">{{ $peg->jabatan->eselon }}</td>
 
                 @php
-                $no++;
+                $jab1=DB::table('riwayat_jab')->where([
+                ['pegawai_id','=',$peg->id],
+                ])->max('tmt');
+
+                $masker=App\Models\gajiberkala::where('pegawai_id','=',$peg->id)
+                ->orderByDesc('id')
+                ->limit(1)
+                ->first();
+
+                $dikjab=App\Models\teknis::where('pegawai_id','=',$peg->id)
+                ->orderByDesc('id')
+                ->limit(1)
+                ->first();
+
+                $dikformal=App\Models\pendidikan::where('pegawai_id','=',$peg->id)
+                ->orderByDesc('id')
+                ->limit(1)
+                ->first();
+
+                $now=now();//tanggal hari ini
+                $t_lahir=$peg->tgl_L; //tanggal lahir
+                $usia_tahun=$t_lahir->diffInYears($now); //menghitung usia dalam tahun
+                $usia_bulan=$t_lahir->diffInMonths($now); //menghitung usia dalam bulan
+                $usia_hari=$t_lahir->diffInDays($now);//menghitung usia dalam hari
+
                 @endphp
+
+                <td style="vertical-align: middle">{{ date('d-m-Y', strtotime($jab1)) }}</td>
+                <td style="vertical-align: middle">Dinas Perhubungan</td>
+                <td style="vertical-align: middle">{{ $peg->masa_kerja_t }}</td>
+                <td style="vertical-align: middle">{{ $peg->masa_kerja_b }}</td>
+                <td style="vertical-align: middle">
+                    @if ($dikjab!=null)
+                    {{ $dikjab->nama_t }};
+                    else
+                    #
+                    @endif
+
+                </td>
+                <td style="vertical-align: middle">
+                    @if ($dikjab!=null)
+                    {{ $dikjab->tgl_m->format('Y.m') }};
+                    else
+                    #
+                    @endif
+
+                </td>
+                <td style="vertical-align: middle">
+                    @if($dikjab !=null)
+                    {{ $dikjab->lama }};
+                    else
+                    #
+                    @endif
+                </td>
+                <td style="vertical-align: middle">
+                    @if($dikformal !=null)
+                    {{ $dikformal->jurusan }};
+                    else
+                    #
+                    @endif
+
+                </td>
+                <td style="vertical-align: middle">
+                    @if($dikformal !=null)
+                    {{ $dikformal->tahun }};
+                    else
+                    #
+                    @endif
+
+                </td>
+                <td style="vertical-align: middle">
+                    @if($dikformal !=null)
+                    {{ $dikformal->gelar }};
+                    else
+                    #
+                    @endif
+
+                </td>
+                <td style="vertical-align: middle">{{ $peg->JK }}</td>
+                <td style="vertical-align: middle">{{ $usia_tahun }}</td>
+                <td style="vertical-align: middle">{{ $usia_bulan }}</td>
+                <td style="vertical-align: middle">{{ $peg->asal_ins }}</td>
+                <td style="vertical-align: middle">#</td>
+            </tr>
+
+            @php
+            $no++;
+            @endphp
 
             @endforeach
         </tbody>
         <tfoot>
         </tfoot>
     </table>
-    <br>
+    
     <div class="row mb-2">
         <div class="col-8"></div>
         <div class="col-4 float-right" style="font-size: 7pt">
             <p style="text-align: center">MUARA BULIAN, {{ now()->isoformat('DD MMMM Y') }}<br />
                 KEPALA DINAS PERHUBUNGAN<br />
                 KABUPATEN BATANG HARI<br>
-                <br>
                 <br>
             </p>
             <p style="text-align: center"><u> {{ strtoupper($kadis->pegawai->Nama) }}</u><br />
@@ -156,5 +213,4 @@
 
     <script type="text/javascript">
         window.addEventListener("load", window.print());
-
     </script>
